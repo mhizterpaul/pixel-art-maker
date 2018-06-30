96dcfbd7ea34cbdae70ba70b1a928458f6b19eec
Paul <chinonsopaul@gmail.com>
2018-06-30 21:56:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pixel Art Maker
+
+This is a Front End Development Project from *Udacity* in collaboration with _ALC_
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+Pixel Art Maker Allows you to create pixel art in your browser:
+* **To use** :
+* Follow this link [https://mhizterpaul.github.io/PixelArtMaker/]()
+* Enter a size for your canvas.
+* Choose a Color.
+* Apply color to Grid cells.
+
+## Contributing
+
+This repository is for academic purpose and will be reviewed by _ALC_ . Therefore, I most likely will not accept pull requests.
---
+++ designs.js
@@ -0,0 +1,55 @@
+/* PIXEL ART MAKER PROJECT FROM ALC FOR FRONT END DEVELOPMENT BEGINNER'S COURSE */
+
+
+// add some jQuery animation on page load
+ $(function(){
+ 	$('form').hide().delay(9000).fadeIn(4800);
+ 	$('.color-content').hide();
+ 	$('h1').hide().delay(4300).show(5);
+    $('body').prepend('<div id="hint"><h3 id="hint-header">Pixel Art Maker</h3><div id="hint-message">Create pixel art right in your browser🎆🎇🎊</div></div>');
+    $('#hint-message').show().delay(2300).fadeOut(2000);
+    $('#hint-message').queue(function(){
+            $('#hint').remove();
+            $(this).dequeue();
+    });
+ });
+
+// Select color input
+// Select size input
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(event) {
+	//declare variables to hold all required information from index.html
+	const gridHeight = Number($('#inputHeight').val()),
+	     gridWidth = Number($('#inputWidth').val()),
+	     tableRow = '<tr  class = "tableRow"></tr>',
+	     tableData = '<td class = "tableData"></td>';
+
+   //remove any table onclick; if any
+	$('.tableRow').remove();
+
+   //dynamically create table based from user input
+    for ( let i = gridHeight; i > 0; i--) {
+	$('#pixelCanvas').append(tableRow);
+}
+    for ( let j = gridWidth; j > 0; j--) {
+	$('.tableRow').append(tableData);
+   } 
+
+   //prevent default action on form  
+   event.preventDefault();
+}
+
+//on submit of form call the makeGrid function and also show color picker
+$('form').submit(makeGrid).one('submit', function(){
+	$(".color-content").show("slow");
+});
+
+//set color of grid cells to  #colorPicker value on click
+$('table').on('click', '.tableData', function(){
+	$(this).css({
+		'background-color' : $('#colorPicker').val()
+	});
+});
+
+    
